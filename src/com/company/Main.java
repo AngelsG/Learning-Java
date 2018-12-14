e4bd438b00a526de5757fc50c6d7dd751a9605fa
Angels Gilabert <angelsgilabert@MacBook-Air-de-Angels.local>
2018-12-14 21:04:31 +0100

    Sum two (2) to any number inputted by the user

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,17 @@
 package com.company;
 
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
-        }
-        int numeroQueHaTriatLUsuari = Integer.parseInt(args[0]);
+        int numeroQueHaTriatLUsuari = Integer.parseInt(readLine(System.in));
         System.out.println("2 + "+numeroQueHaTriatLUsuari + " = " + (2 + numeroQueHaTriatLUsuari));
+    }
+
+    private static String readLine(InputStream in) {
+        return new Scanner(in).nextLine();
     }
 }
 
