4f38ca3f251253428277bd42372539b70115d705
Angels Gilabert <angelsgilabert@MacBook-Air-de-Angels.local>
2019-01-22 21:23:00 +0100

    imprimir tots els interviewers, començant per un d'aleatori, cada cop que en demanem un

--- src/com/company/Pairing.java
+++ src/com/company/Pairing.java
@@ -3,6 +3,7 @@ package com.company;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Pairing {
     public static void main(String[] args) {
@@ -12,9 +13,18 @@ public class Pairing {
         interviwers.add("David");
         interviwers.add("Tom");
 
-        int randomPerson = new Random().nextInt(4);
+        Random random = new Random();
 
-        System.out.print(interviwers.get(randomPerson));
+        int randomPerson = random.nextInt(4);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            scanner.nextLine();
+            if(randomPerson == 4){
+                randomPerson = 0;
+            }
+            System.out.print(interviwers.get(randomPerson));
+            randomPerson = randomPerson + 1;
+        }
 
     }
 }
